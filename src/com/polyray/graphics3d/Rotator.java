e7f9282774124619ae1f95f31f46ff0dc0059073
Graph3r <>
2024-04-14 22:13:28 +0200

    made the axis constants final

--- src/com/polyray/graphics3d/Rotator.java
+++ src/com/polyray/graphics3d/Rotator.java
@@ -2,7 +2,10 @@ package com.polyray.graphics3d;
 
 public class Rotator {
 
-    private Vector3f a = new Vector3f(1.0f, 0.0f, 0.0f), b = new Vector3f(0.0f, 1.0f, 0.0f), c = new Vector3f(0.0f, 0.0f, 1.0f);
+    private final Vector3f a = new Vector3f(1.0f, 0.0f, 0.0f);
+    private final Vector3f b = new Vector3f(0.0f, 1.0f, 0.0f);
+    private final Vector3f c = new Vector3f(0.0f, 0.0f, 1.0f);
+    
     public Vector3f calcRot(Vector3f v, Vector3f ang, int prioAxis) {
         Vector3f nV = new Vector3f(0.0f, 0.0f, 0.0f);
         switch (prioAxis) {
@@ -54,22 +57,22 @@ public class Rotator {
     private Vector3f rotate(Vector3f k, Vector3f v, float ang) {
         //return Vector3f.add(Vector3f.mul(v, (float) Math.cos(ang)), Vector3f.add(Vector3f.mul(Vector3f.cross(k, v), (float) Math.sin(ang)), Vector3f.mul(k, Vector3f.dot(k, v) * (float) (1.0 - Math.cos(ang)))));
         float sinHalfAngle = (float) Math.sin(ang / 2.0);
-    float cosHalfAngle = (float) Math.cos(ang / 2.0);
+        float cosHalfAngle = (float) Math.cos(ang / 2.0);
 
-    float rx = k.x * sinHalfAngle;
-    float ry = k.y * sinHalfAngle;
-    float rz = k.z * sinHalfAngle;
-    float rw = cosHalfAngle;
+        float rx = k.x * sinHalfAngle;
+        float ry = k.y * sinHalfAngle;
+        float rz = k.z * sinHalfAngle;
+        float rw = cosHalfAngle;
 
-    float ix = rw * v.x + ry * v.z - rz * v.y;
-    float iy = rw * v.y + rz * v.x - rx * v.z;
-    float iz = rw * v.z + rx * v.y - ry * v.x;
-    float iw = -rx * v.x - ry * v.y - rz * v.z;
+        float ix = rw * v.x + ry * v.z - rz * v.y;
+        float iy = rw * v.y + rz * v.x - rx * v.z;
+        float iz = rw * v.z + rx * v.y - ry * v.x;
+        float iw = -rx * v.x - ry * v.y - rz * v.z;
 
-    return new Vector3f(
-        ix * rw + iw * -rx + iy * -rz - iz * -ry,
-        iy * rw + iw * -ry + iz * -rx - ix * -rz,
-        iz * rw + iw * -rz + ix * -ry - iy * -rx
-    );
+        return new Vector3f(
+                ix * rw + iw * -rx + iy * -rz - iz * -ry,
+                iy * rw + iw * -ry + iz * -rx - ix * -rz,
+                iz * rw + iw * -rz + ix * -ry - iy * -rx
+        );
     }
 }
