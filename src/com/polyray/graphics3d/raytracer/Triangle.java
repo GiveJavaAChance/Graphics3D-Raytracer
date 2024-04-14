4e46df448cf547d27158b67c5b6da4fa3b15875e
Graph3r <>
2024-04-14 22:19:53 +0200

    improved texture mapping and added u, v coordinates to properly align texture

--- src/com/polyray/graphics3d/raytracer/Triangle.java
+++ src/com/polyray/graphics3d/raytracer/Triangle.java
@@ -1,5 +1,6 @@
 package com.polyray.graphics3d.raytracer;
 
+import com.polyray.graphics3d.Vector2f;
 import com.polyray.graphics3d.Vector3f;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
@@ -8,8 +9,10 @@ import java.util.ArrayList;
 public class Triangle {
 
     public Vector3f a, b, c;
+    public Vector2f ta, tb, tc;
     public ColorObject col;
     private BufferedImage texture;
+    private int width, height;
     public boolean hasTexture = false;
 
     public Triangle(Vector3f a, Vector3f b, Vector3f c, ColorObject col) {
@@ -48,42 +51,48 @@ public class Triangle {
         if (texture == null) {
             return null;
         }
-        /*Vector3f v0 = Vector3f.sub(this.b, this.a);
-        Vector3f v1 = Vector3f.sub(this.c, this.a);
-        Vector3f v2 = Vector3f.sub(pos, this.a);
+        Vector3f v = toBary(pos);
+        if (v.x < 0.0f || v.x > 1.0f || v.y < 0.0f || v.y > 1.0f || v.z < 0.0f || v.z > 1.0f) {
+            return new ColorObject(0.0f, 0.0f, 0.0f);
+        }
+        Vector2f p = Vector2f.add(Vector2f.mul(ta, v.x), Vector2f.add(Vector2f.mul(tb, v.y), Vector2f.mul(tc, v.z)));
+        int x = Math.min(Math.max((int) (p.x * width), 0), width - 1);
+        int y = Math.min(Math.max((int) (p.y * height), 0), height - 1);
+        return ColorObject.toColorObject(new Color(texture.getRGB(x, y)));
+    }
+
+    private Vector3f toBary(Vector3f P) {
+        Vector3f v0 = Vector3f.sub(b, a);
+        Vector3f v1 = Vector3f.sub(c, a);
+        Vector3f v2 = Vector3f.sub(P, a);
         float dot00 = Vector3f.dot(v0, v0);
         float dot01 = Vector3f.dot(v0, v1);
         float dot02 = Vector3f.dot(v0, v2);
         float dot11 = Vector3f.dot(v1, v1);
         float dot12 = Vector3f.dot(v1, v2);
-        float denom = (dot00 * dot11 - dot01 * dot01);
-        float u = (dot11 * dot02 - dot01 * dot12) / denom;
-        float v = (dot00 * dot12 - dot01 * dot02) / denom;
-        float w = 1.0f - u - v;*/
-        // Calculate the vectors AB and AC
-        Vector3f AB = Vector3f.sub(this.b, this.a);
-        Vector3f AC = Vector3f.sub(this.c, this.a);
-
-        // Calculate the lengths of sides AB and AC
-        float lengthAB = Vector3f.length(AB);
-        float lengthAC = Vector3f.length(AC);
-
-        // Calculate the dot products for projections
-        float dotAB = Vector3f.dot(AB, Vector3f.sub(pos, this.a));
-        float dotAC = Vector3f.dot(AC, Vector3f.sub(pos, this.a));
-
-        // Calculate normalized coordinates
-        float xNorm = dotAB / (lengthAB * lengthAB);
-        float yNorm = dotAC / (lengthAC * lengthAC);
-        float x = xNorm * texture.getWidth();
-        float y = yNorm * texture.getHeight();
-        int X = Math.max(Math.min((int) x, texture.getWidth()), 0);
-        int Y = Math.max(Math.min((int) y, texture.getHeight()), 0);
-        return ColorObject.toColorObject(new Color(texture.getRGB(X, Y)));
+        float invDenom = 1.0f / (dot00 * dot11 - dot01 * dot01);
+        float u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+        float v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+        float w = 1.0f - u - v;
+        return new Vector3f(u, v, w);
     }
 
-    public void setTexture(BufferedImage texture) {
+    public void setTexture(BufferedImage texture, float u1, float v1, float u2, float v2, float u3, float v3) {
         this.texture = texture;
+        width = texture.getWidth();
+        height = texture.getHeight();
         hasTexture = true;
+        /*this.to = new Vector2f(u1, v1);
+        Vector2f tb = new Vector2f(u2, v2);
+        Vector2f tc = new Vector2f(u3, v3);
+        this.tx = Vector2f.sub(tb, to);
+        this.ty = Vector2f.sub(tc, to);*/
+        this.ta = new Vector2f(u1, v1);
+        this.tb = new Vector2f(u2, v2);
+        this.tc = new Vector2f(u3, v3);
+    }
+
+    public boolean hasTeture() {
+        return this.hasTexture && this.texture != null;
     }
 }
